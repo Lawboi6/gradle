omeos-fork/gradle.gradle

Migrate Copy.destinationDir and Sync.destinationDir to lazy property

platforms/core-configuration/core-serialization-codecs/src/main/kotlin/org/gradle/internal/serialize/codecs/core/DefaultCopySpecCodec.kt
@@ -27,7 +27,9 @@ import org.gradle.api.internal.file.copy.CopySpecInternal
 import org.gradle.api.internal.file.copy.DefaultCopySpec
 import org.gradle.api.model.ObjectFactory
 import org.gradle.api.tasks.util.PatternSet
+import org.gradle.internal.Factory
 import org.gradle.internal.extensions.stdlib.uncheckedCast
+import org.gradle.internal.reflect.Instantiator
 import org.gradle.internal.serialize.graph.Codec
 import org.gradle.internal.serialize.graph.ReadContext
 import org.gradle.internal.serialize.graph.WriteContext
@@ -36,9 +38,8 @@ import org.gradle.internal.serialize.graph.encodePreservingIdentityOf
 import org.gradle.internal.serialize.graph.readEnum
 import org.gradle.internal.serialize.graph.readList
 import org.gradle.internal.serialize.graph.writeCollection
-import org.gradle.internal.Factory
 import org.gradle.internal.serialize.graph.writeEnum
-import org.gradle.internal.reflect.Instantiator
+import java.io.File
 
 
 class DefaultCopySpecCodec(
@@ -51,7 +52,8 @@ class DefaultCopySpecCodec(
 
     override suspend fun WriteContext.encode(value: DefaultCopySpec) {
         encodePreservingIdentityOf(value) {
-            writeNullableString(value.destinationDir?.path)
+            // TODO: Should we write this with DirectoryPropertyCodec?
+            writeNullableString(value.destinationDir.asFile.map { it.absolutePath }.orNull)
             write(value.sourceRootsForThisSpec)
             write(value.patterns)
             writeEnum(value.duplicatesStrategyForThisSpec)
@@ -78,7 +80,10 @@ class DefaultCopySpecCodec(
             val fileMode = readNullableSmallInt()
             val actions = readList().uncheckedCast<List<Action<FileCopyDetails>>>()
             val children = readList().uncheckedCast<List<CopySpecInternal>>()
-            val copySpec = DefaultCopySpec(fileCollectionFactory, objectFactory, instantiator, patternSetFactory, destPath, sourceFiles, patterns, actions, children)
+            val copySpec = DefaultCopySpec(fileCollectionFactory, objectFactory, instantiator, patternSetFactory, sourceFiles, patterns, actions, children)
+            if (destPath != null) {
+                copySpec.destinationDir.set(File(destPath))
+            }
             copySpec.duplicatesStrategy = duplicatesStrategy
             copySpec.includeEmptyDirs = includeEmptyDirs
             copySpec.isCaseSensitive = isCaseSensitive

platforms/core-configuration/file-operations/src/main/java/org/gradle/api/internal/file/copy/FileCopier.java
@@ -67,7 +67,7 @@ public class FileCopier {
 
     private DestinationRootCopySpec createCopySpec(Action<? super SyncSpec> action) {
         DefaultCopySpec copySpec = new DefaultCopySpec(fileCollectionFactory, objectFactory, instantiator, patternSetFactory);
-        DestinationRootCopySpec destinationRootCopySpec = new DestinationRootCopySpec(fileResolver, copySpec);
+        DestinationRootCopySpec destinationRootCopySpec = objectFactory.newInstance(DestinationRootCopySpec.class, fileResolver, copySpec);
         SyncSpec wrapped = instantiator.newInstance(CopySpecWrapper.class, destinationRootCopySpec);
         action.execute(wrapped);
         return destinationRootCopySpec;
@@ -75,13 +75,13 @@ public class FileCopier {
 
     public WorkResult copy(Action<? super CopySpec> action) {
         DestinationRootCopySpec copySpec = createCopySpec(action);
-        File destinationDir = copySpec.getDestinationDir();
+        File destinationDir = copySpec.getDestinationDir().getAsFile().get();
         return doCopy(copySpec, getCopyVisitor(destinationDir));
     }
 
     public WorkResult sync(Action<? super SyncSpec> action) {
         DestinationRootCopySpec copySpec = createCopySpec(action);
-        File destinationDir = copySpec.getDestinationDir();
+        File destinationDir = copySpec.getDestinationDir().getAsFile().get();
         return doCopy(copySpec,
             new SyncCopyActionDecorator(
                 destinationDir,

subprojects/core/src/test/groovy/org/gradle/api/internal/file/copy/CopyActionExecuterTest.groovy
@@ -37,7 +37,7 @@ class CopyActionExecuterTest extends WorkspaceTest {
 
         def resolver = TestFiles.resolver(testDirectory)
         def fileCollectionFactory = TestFiles.fileCollectionFactory(testDirectory)
-        def copySpec = new DestinationRootCopySpec(resolver, new DefaultCopySpec(fileCollectionFactory, TestUtil.objectFactory(), TestUtil.instantiatorFactory().decorateLenient(), TestFiles.patternSetFactory))
+        def copySpec = TestUtil.newInstance(DestinationRootCopySpec, resolver, new DefaultCopySpec(fileCollectionFactory, TestUtil.objectFactory(), TestUtil.instantiatorFactory().decorateLenient(), TestFiles.patternSetFactory))
         copySpec.with {
             into "out"
             from "a", {

Lawboi6/gradle

platforms/core-configuration/core-serialization-codecs/src/main/kotlin/org/gradle/internal/serialize/codecs/core/DefaultCopySpecCodec.kt
@@ -27,7 +27,9 @@ import org.gradle.api.internal.file.copy.CopySpecInternal
 import org.gradle.api.internal.file.copy.DefaultCopySpec
 import org.gradle.api.internal.provider.PropertyFactory
 import org.gradle.api.tasks.util.PatternSet
+import org.gradle.internal.Factory
 import org.gradle.internal.extensions.stdlib.uncheckedCast
+import org.gradle.internal.reflect.Instantiator
 import org.gradle.internal.serialize.graph.Codec
 import org.gradle.internal.serialize.graph.ReadContext
 import org.gradle.internal.serialize.graph.WriteContext
@@ -36,9 +38,7 @@ import org.gradle.internal.serialize.graph.encodePreservingIdentityOf
 import org.gradle.internal.serialize.graph.readEnum
 import org.gradle.internal.serialize.graph.readList
 import org.gradle.internal.serialize.graph.writeCollection
-import org.gradle.internal.Factory
 import org.gradle.internal.serialize.graph.writeEnum
-import org.gradle.internal.reflect.Instantiator
 
 
 class DefaultCopySpecCodec(
@@ -51,7 +51,7 @@ class DefaultCopySpecCodec(
 
     override suspend fun WriteContext.encode(value: DefaultCopySpec) {
         encodePreservingIdentityOf(value) {
-            writeNullableString(value.destinationDir?.path)
+            writeNullableString(value.destPath)
             write(value.sourceRootsForThisSpec)
             write(value.patterns)
             writeEnum(value.duplicatesStrategyForThisSpec)
@@ -78,7 +78,8 @@ class DefaultCopySpecCodec(
             val fileMode = readNullableSmallInt()
             val actions = readList().uncheckedCast<List<Action<FileCopyDetails>>>()
             val children = readList().uncheckedCast<List<CopySpecInternal>>()
-            val copySpec = DefaultCopySpec(fileCollectionFactory, propertyFactory, instantiator, patternSetFactory, destPath, sourceFiles, patterns, actions, children)
+            val copySpec = DefaultCopySpec(fileCollectionFactory, propertyFactory, instantiator, patternSetFactory, sourceFiles, patterns, actions, children)
+            destPath?.let { copySpec.into(destPath) }
             copySpec.duplicatesStrategy = duplicatesStrategy
             copySpec.includeEmptyDirs = includeEmptyDirs
             copySpec.isCaseSensitive = isCaseSensitive

platforms/core-configuration/file-operations/src/main/java/org/gradle/api/internal/file/copy/FileCopier.java
@@ -67,7 +67,7 @@ public class FileCopier {
 
     private DestinationRootCopySpec createCopySpec(Action<? super SyncSpec> action) {
         DefaultCopySpec copySpec = new DefaultCopySpec(fileCollectionFactory, propertyFactory, instantiator, patternSetFactory);
-        DestinationRootCopySpec destinationRootCopySpec = new DestinationRootCopySpec(fileResolver, copySpec);
+        DestinationRootCopySpec destinationRootCopySpec = objectFactory.newInstance(DestinationRootCopySpec.class, fileResolver, copySpec);
         SyncSpec wrapped = instantiator.newInstance(CopySpecWrapper.class, destinationRootCopySpec);
         action.execute(wrapped);
         return destinationRootCopySpec;
@@ -75,13 +75,13 @@ public class FileCopier {
 
     public WorkResult copy(Action<? super CopySpec> action) {
         DestinationRootCopySpec copySpec = createCopySpec(action);
-        File destinationDir = copySpec.getDestinationDir();
+        File destinationDir = copySpec.getDestinationDir().get().getAsFile();
         return doCopy(copySpec, getCopyVisitor(destinationDir));
     }
 
     public WorkResult sync(Action<? super SyncSpec> action) {
         DestinationRootCopySpec copySpec = createCopySpec(action);
-        File destinationDir = copySpec.getDestinationDir();
+        File destinationDir = copySpec.getDestinationDir().get().getAsFile();
         return doCopy(copySpec,
             new SyncCopyActionDecorator(
                 destinationDir,

subprojects/core/src/test/groovy/org/gradle/api/internal/file/copy/CopyActionExecuterTest.groovy
@@ -37,7 +37,7 @@ class CopyActionExecuterTest extends WorkspaceTest {
 
         def resolver = TestFiles.resolver(testDirectory)
         def fileCollectionFactory = TestFiles.fileCollectionFactory(testDirectory)
-        def copySpec = new DestinationRootCopySpec(resolver, new DefaultCopySpec(fileCollectionFactory, TestUtil.propertyFactory(), TestUtil.instantiatorFactory().decorateLenient(), TestFiles.patternSetFactory))
+        def copySpec = TestUtil.newInstance(DestinationRootCopySpec, resolver, new DefaultCopySpec(fileCollectionFactory, TestUtil.propertyFactory(), TestUtil.instantiatorFactory().decorateLenient(), TestFiles.patternSetFactory))
         copySpec.with {
             into "out"
             from "a", {